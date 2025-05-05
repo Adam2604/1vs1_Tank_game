3d73dc9a4b3096d8c9307e9ba30c1264c9061bbb
Adam2604 <adammichala11@gmail.com>
2025-05-05 12:42:39 +0200

    Poprawa centrowania napisów

--- button.py
+++ button.py
@@ -1,27 +1,28 @@
 class Button:
-	def __init__(self, image, position, label, font, normal_color, hover_color):
-		self.image = image
-		self.position = position
-		self.font = font
-		self.label_text = label
-		self.normal_color = normal_color
-		self.hover_color = hover_color
+    def __init__(self, image, position, label, font, normal_color, hover_color):
+        self.image = image
+        self.position = position
+        self.font = font
+        self.label_text = label
+        self.normal_color = normal_color
+        self.hover_color = hover_color
 
-		self.text_surface = self.font.render(self.label_text, True, self.normal_color)
+        self.text_surface = self.font.render(self.label_text, True, self.normal_color)
 
-		if self.image is None:
-			self.image = self.text_surface
+        if self.image is None:
+            self.image = self.text_surface
 
-		self.image_rect = self.image.get_rect(center=self.position)
-		self.text_rect = self.text_surface.get_rect(center=self.position)
+        self.image_rect = self.image.get_rect(center=self.position)
+        self.text_rect = self.text_surface.get_rect(center=self.image_rect.center)
 
-	def draw(self, screen):
-		screen.blit(self.image, self.image_rect)
-		screen.blit(self.text_surface, self.text_rect)
+    def draw(self, screen):
+        screen.blit(self.image, self.image_rect)
+        screen.blit(self.text_surface, self.text_rect)
 
-	def is_clicked(self, mouse_pos):
-		return self.image_rect.collidepoint(mouse_pos)
+    def is_clicked(self, mouse_pos):
+        return self.image_rect.collidepoint(mouse_pos)
 
-	def update_color(self, mouse_pos):
-		color = self.hover_color if self.image_rect.collidepoint(mouse_pos) else self.normal_color
-		self.text_surface = self.font.render(self.label_text, True, color)
+    def update_color(self, mouse_pos):
+        color = self.hover_color if self.image_rect.collidepoint(mouse_pos) else self.normal_color
+        self.text_surface = self.font.render(self.label_text, True, color)
+        self.text_rect = self.text_surface.get_rect(center=self.image_rect.center)
